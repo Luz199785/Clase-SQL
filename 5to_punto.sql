ab3c25637fae600784e777152fd1f0729d205649
michaelbogota <34754049+michaelbogota@users.noreply.github.com>
2022-10-12 20:34:30 -0500

    Create 5to_punto.sql

---
+++ 5to_punto.sql
@@ -0,0 +1,5 @@
+select sum(ta.codigo) as total, c.nombre as ciudad, ta.nombre as tipo from afiliados as a
+inner join tipo_afiliaciones ta on a.tip_afil_codigo = ta.codigo
+inner join ciudades c on c.codigo = a.ciu_direc_codigo
+where ta.nombre = 'COTIZANTES'
+group by c.nombre, ta.nombre;
